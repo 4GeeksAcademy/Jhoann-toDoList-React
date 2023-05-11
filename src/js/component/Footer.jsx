3731b451be6a59057d4a05741189b2290cf1b499
Jhoann <64670851+Jansooxx@users.noreply.github.com>
2023-05-11 02:22:10 +0000

    contact list primer ajuste

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = ({ counter }) => {
+
+    return (
+    <div className="footer">
+      <p>{counter} item(s) left</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/js/component/Lista.jsx
@@ -0,0 +1,85 @@
+import React, {useState} from 'react'
+import Footer from "./Footer.jsx";
+
+
+const Lista = () => {
+// STATES
+  const [contentInput, setContentInput] = useState("");
+  const [valuesKey, setValuesKey] = useState([]);
+  const [elements,setElements] = useState(-1);
+  const counter = valuesKey.length
+
+
+
+
+  //FUNCTIONS STATES
+
+  const handleEnter = (event) => {
+    if(event.key === "Enter"){
+      setValuesKey([...valuesKey,contentInput]);
+      setContentInput("");
+    }
+  };
+
+  const handleMouseEnter = (index) => {
+    setElements(index);
+  };
+  const handleMouseLeave = () => {
+    setElements(-1);
+  };
+
+  const removeElements = (index) => {
+    const newValuesKey = [...valuesKey];
+    newValuesKey.splice(index, 1);
+    setValuesKey(newValuesKey);
+  };
+  return (
+
+    <>
+          <h1 className="text-center mt-5 fs-1">todos</h1>
+
+          <div className='container-fluid z-3 text-center sombras mb-3 bg-body-tertiary  position-relative w-75 border border-dark-subtle border border-1 border-dark-subtle  mb-2 border-opacity-50 px-0'>
+
+          <div className='row inpList w-100 m-0 '>
+            <input 
+                className=' border-bottom py-2 border-opacity-75 fs-4'
+                type="text" 
+                placeholder='What needs to be done?' 
+                onChange={(evento)=> setContentInput(evento.target.value.trim())}
+                onKeyDown={handleEnter}
+                value={contentInput}
+                />
+
+            <ul className="px-0 pt-0 pb-0 py-2 unOrList w-100 "> {valuesKey.map((obj,index) =>
+            (<li
+                className='border-bottom border-opacity-75 w-100 px-4'
+                key={index}
+                onMouseEnter={() => handleMouseEnter(index)} 
+                onMouseLeave={handleMouseLeave}>{obj}
+                {elements === index && ( 
+                  <button 
+                    className="closer" 
+                    onClick={() => removeElements(index)}>
+                  x
+                </button>
+                  )}  
+            </li>
+            ))}
+            <Footer
+              counter={counter} 
+            />
+            
+            </ul>
+          </div>
+          </div>
+            <div className='barra'></div>
+            <div className='barra2'></div>
+          
+            
+      
+        
+    </>
+  )
+}
+
+export default Lista
